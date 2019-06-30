c9bc767f64ace174b3d57e74f93edcd5fcabfcd1
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-06-30 10:23:04 +0300

    Tools

---
+++ FocLab/MigrationTool/Tools/AddDbFileHistory.cs
@@ -0,0 +1,52 @@
+﻿using Croco.Core.Abstractions.ContextWrappers;
+using Croco.Core.Common.Models;
+using FocLab.Model.Contexts;
+using FocLab.Model.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MigrationTool.Tools
+{
+    public class AddDbFileHistory
+    {
+        public AddDbFileHistory(IUserContextWrapper<ChemistryDbContext> context)
+        {
+            Context = context;
+        }
+
+        public IUserContextWrapper<ChemistryDbContext> Context { get; }
+
+        public async Task<BaseApiResponse> Execute()
+        {
+            int count = 10;
+
+            var doneBatchesCount = await ExecuteBatch(count);
+
+            return new BaseApiResponse(true, $"Выполнено преобразований {doneBatchesCount}");
+        }
+
+        public async Task<int> ExecuteBatch(int count)
+        {
+            var filesWithNoHistory = Context.DbContext.DbFiles.Where(x => !x.History.Any())
+                .OrderBy(x => x.Id)
+                .Take(count);
+            
+            var histories = filesWithNoHistory.Select(x => new ApplicationDbFileHistory
+            {
+                Id = Guid.NewGuid().ToString(),
+                FileData = x.FileData,
+                FileName = x.FileName,
+                FilePath = x.FilePath,
+                ParentId = x.Id
+            }).ToList();
+
+            var repo = Context.GetRepository<ApplicationDbFileHistory>();
+
+            repo.CreateHandled(histories);
+            await Context.SaveChangesAsync();
+
+            return histories.Count;
+        }
+    }
+}
