eaea667dbf89bdce18ecafb827180266607e3e62
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-07-04 14:54:11 +0300

    Fixes

--- FocLab/FocLab/Startup.cs
+++ FocLab/FocLab/Startup.cs
@@ -88,19 +88,14 @@ namespace FocLab
                 c.SwaggerDoc("v1", new Info
                 {
                     Version = "v1",
-                    Title = "ToDo API",
-                    Description = "A simple example ASP.NET Core Web API",
+                    Title = "FocLab API",
+                    Description = "FocLab ASP.NET Core Web API",
                     TermsOfService = "None",
                     Contact = new Contact
                     {
-                        Name = "Shayne Boyer",
-                        Email = string.Empty,
-                        Url = "https://twitter.com/spboyer"
-                    },
-                    License = new License
-                    {
-                        Name = "Use under LICX",
-                        Url = "https://example.com/license"
+                        Name = "Dmitry Serdyukov",
+                        Email = "dimaserd96@yandex.ru",
+                        Url = null
                     }
                 });
                 c.EnableAnnotations();
