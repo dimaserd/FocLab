475bf5370ef587b38ed50a40adca5b06a46c1bbd
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-09-09 22:59:45 +0300

    Fixes

--- FocLab/FocLab/Controllers/Mvc/ScheduleController.cs
+++ FocLab/FocLab/Controllers/Mvc/ScheduleController.cs
@@ -3,15 +3,20 @@ using FocLab.Logic.Services;
 using FocLab.Model.Contexts;
 using Microsoft.AspNetCore.Mvc;
 using Tms.Logic.Models.Tasker;
-using Microsoft.AspNetCore.Http;
 using Tms.Logic.Workers.Tasker;
 using System.Threading.Tasks;
+using Croco.Core.Application;
+using FocLab.Model.Entities.Tasker;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using FocLab.Logic.Models.Users;
+using Zoo.GenericUserInterface.Models;
 
 namespace FocLab.Controllers.Mvc
 {
     public class ScheduleController : BaseController
     {
-        public ScheduleController(ChemistryDbContext context, ApplicationUserManager userManager, ApplicationSignInManager signInManager, IHttpContextAccessor contextAccessor) : base(context, userManager, signInManager)
+        public ScheduleController(ChemistryDbContext context, ApplicationUserManager userManager, ApplicationSignInManager signInManager) : base(context, userManager, signInManager)
         {
         }
 
@@ -34,6 +39,19 @@ namespace FocLab.Controllers.Mvc
         public async Task<IActionResult> Task(string id)
         {
             var model = await TasksWorker.GetDayTaskByIdAsync(id);
+
+            var task = await TasksWorker.DbContext.Set<ApplicationDayTask>().FirstOrDefaultAsync(x => x.Id == id);
+
+            var users = await UserSearcher.GetUsersAsync(UserSearch.GetAllUsers);
+
+            var userSelectList = users.List.Select(x => new MySelectListItem
+            {
+                Selected = x.Id == task.AssigneeUserId,
+                Text = x.Email,
+                Value = x.Id
+            });
+
+            var history = await CrocoApp.Application.GetAuditService().GetAuditData(task, TasksWorker.DbContext);
 
             return View(model);
         }
