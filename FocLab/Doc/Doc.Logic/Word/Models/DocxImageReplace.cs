acad5ccb827f1cd9283f3aeba4cd8bd15197b8f9
dimaserd84@gmail.com <dimaserd84@gmail.com>
2021-04-02 14:35:59 +0300

    Fixes

--- FocLab/Doc/Doc.Logic/Word/Models/DocxImageReplace.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Doc.Logic.Word.Models
-{
-    public class DocxImageReplace
-    {
-        public string TextToReplace { get; set; }
-
-        public string ImageFilePath { get; set; }
-    }
-}
--- FocLab/Doc/Doc.Logic/Word/Models/EmuRectangle.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Doc.Logic.Word.Models
-{
-    public class EmuRectangle
-    {
-        public long Height { get; set; }
-
-        public long Width { get; set; }
-    }
-}
